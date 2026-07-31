eb415e180cb4c555580b151d345e1fa180a31d20
annguyeniale17-hub <annguyeniale17@gmail.com>
2026-08-01 01:28:39 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+// Complete App Code exported from Canvas
+function App() {
+    return (
+        <div>
+            <h1>Coffee Management System</h1>
+        </div>
+    );
+}
+
+export default App;
